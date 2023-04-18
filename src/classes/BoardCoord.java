fbba68cdd84fd6d679d0ddaf9408426760c1b6b4
Fernando Igor <fernando_id.7@hotmail.com>
2023-04-18 16:42:38 -0300

    Override Equals on Match Class

--- src/classes/BoardCoord.java
+++ src/classes/BoardCoord.java
@@ -1,8 +1,8 @@
-package com.dsproject.classes;
+package classes;
 
 public class BoardCoord {
-    int coord1; 
-    int coord2;
+    private int coord1; 
+    private int coord2;
 
     void setCoord1(int coord1){
         this.coord1 = coord1;
@@ -15,5 +15,21 @@ public class BoardCoord {
     }
     public int getCoord2() {
         return this.coord2;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+       
+        if(this == obj){
+            return true;
+        }
+
+        if(!(obj instanceof BoardCoord)){
+            return false;
+        }
+
+        BoardCoord b = (BoardCoord) obj;
+
+        return this.getCoord1() == b.getCoord1() && this.getCoord2() == b.getCoord2();
     }
 }
